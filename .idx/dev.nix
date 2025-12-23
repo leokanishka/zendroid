6ab0a825ac7b51468a7c32028c642ea622dc5d15
BaatcheetCreator <creator@baatcheetpodact.com>
2025-12-23 18:50:47 +0530

    Fix IDX: Set JAVA_HOME and update nixpkgs channel

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,16 +1,18 @@
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-23.11"; # or "unstable"
+  channel = "stable-24.05"; # Updated for better Android support
 
   # Use https://search.nixos.org/packages to find packages
-  packages = with pkgs; [
-    gradle
-    jdk17
-    android-tools
+  packages = [
+    pkgs.gradle
+    pkgs.jdk17
+    pkgs.android-tools
   ];
 
   # Sets environment variables in the workspace
-  env = {};
+  env = {
+    JAVA_HOME = "${pkgs.jdk17}";
+  };
 
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
